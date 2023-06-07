e6a71724f770fa7518d6f38cd0967a2a7075fbd8
deepthichathurani64@gmail.com <deepthichathurani64@gmail.com>
2023-06-07 23:33:33 +0530

    Added

--- src/lk/ijse/livechat/LoginInitializer.java
+++ src/lk/ijse/livechat/LoginInitializer.java
@@ -2,22 +2,20 @@ package lk.ijse.livechat;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-public class ServerInitializer extends Application {
+public class LoginInitializer extends Application {
     public static void main(String[] args) {
-              launch(args);
+        launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene
                 (new Scene(FXMLLoader.
                         load(getClass().
-                                getResource("/lk/ijse/livechat/view/Server.fxml"))));
-        primaryStage.setTitle("Server");
+                                getResource("/lk/ijse/livechat/view/Login.fxml"))));
+        primaryStage.setTitle("Login ");
         primaryStage.show();
     }
 }
