6757961b30ffde347cbb54ff756c1dd30c8046da
deepthichathurani64@gmail.com <deepthichathurani64@gmail.com>
2023-06-07 05:57:11 +0530

    client2 added

---
+++ src/lk/ijse/livechat/controller/Client2Controller.java
@@ -0,0 +1,28 @@
+package lk.ijse.livechat.controller;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class Client2Controller {
+    public AnchorPane pane;
+    public ScrollPane scrollPane;
+    public VBox txtOutPut;
+    public TextField txtInput;
+    public ImageView minimize;
+
+    public void btnClose(MouseEvent mouseEvent) {
+    }
+
+    public void btnSend(MouseEvent mouseEvent) {
+    }
+
+    public void btnCamera(MouseEvent mouseEvent) {
+    }
+
+    public void btnMinimize(MouseEvent mouseEvent) {
+    }
+}
